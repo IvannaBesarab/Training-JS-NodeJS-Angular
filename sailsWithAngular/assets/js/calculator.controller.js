ebdfa60600e1a3449c635cbe96969ff5652ea293
Ivanna Besarab <ivanna@besarab.com>
2016-07-05 02:47:59 +0300

    added angular calculator to Sails folder

---
+++ sailsWithAngular/assets/js/calculator.controller.js
@@ -0,0 +1,69 @@
+(function(){
+	'use strict';
+
+	angular
+		.module('calculatorApp')
+		// Values
+		.value('numbers', [ [1, 2, 3],[4, 5, 6], [7, 8, 9], [0]] )
+		.value('operants', [['+', '-'], ['/', '*']] )
+		// Directive Controller
+		.controller('CalculatorController', CalculatorController)
+
+	CalculatorController.$inject = ['numbers', 'operants'];
+
+	function CalculatorController(numbers, operants){
+		var cl = this;
+		var current = '';
+		var memory = 0;
+		var operant = ''
+		cl.typedExpression = '';
+		cl.result = 0;
+		cl.pressKey = pressKey;
+		cl.pressOperant = pressOperant;
+		cl.calculate = calculate;
+		cl.numbers = numbers;
+		cl.operants = operants;
+
+
+		function pressKey(key){
+			console.log(key)
+			cl.typedExpression += key;
+			current += key;
+		}
+
+		function pressOperant(key){
+			console.log(key)
+			cl.typedExpression += key;
+			operant = key;
+			memory = Number.parseFloat(current);
+			current = '';
+		}
+
+		function calculate(){
+			if(current){
+				current = Number.parseFloat(current);
+
+				if(operant === '+'){
+					cl.result = memory + current;
+				}
+				if(operant === '-'){
+					cl.result = memory - current;
+				}
+				if(operant === '*'){
+					cl.result = memory * current;
+				}
+				if(operant === '/'){
+					cl.result = memory / current;
+				}
+			}
+
+			cleanVariables();
+		}
+
+		function cleanVariables(){
+			current = '';
+			cl.typedExpression = '';
+		}
+	}
+
+})();
